3fafa38bb897c4186e67e57a12c0102e6fbd62b3
Marco Pavan <marco.pavan@vivabiocell.it>
2022-03-15 11:56:41 +0100

    Other final integration to the 3 commits before

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,5 +1,43 @@
+const { query } = require('express')
+const https = require('https')
+let stocks = []
+
+function getStocksList () {
+  const options = {
+    method: 'GET',
+    hostname: 'tradingradar.p.rapidapi.com',
+    port: null,
+    path: '/api/stocks',
+    headers: {
+      'x-rapidapi-host': 'tradingradar.p.rapidapi.com',
+      'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
+      useQueryString: true
+    }
+  }
+  const request = https.request(options, function (response) {
+    const chunks = []
+    response.on('data', function (chunk) {
+      chunks.push(chunk)
+    })
+    response.on('end', function () {
+      const body = Buffer.concat(chunks).toString()
+      try {
+        stocks = JSON.parse(body)
+      } catch (error) {
+        console.err(error)
+      }
+    })
+  })
+  request.end()
+  request.on('error', error => {
+    console.err(error)
+  })
+}
+getStocksList()
+
 module.exports = app => {
   app.get('/', (req, res) => {
+    res.locals.stocks = stocks
     res.render('index', { id: 'home', title: 'Home' })
   })
   app.get('/privacy', (req, res) => {
@@ -7,5 +45,8 @@ module.exports = app => {
   })
   app.get('/contatti', (req, res) => {
     res.render('contatti/contatti', { id: 'contacts', title: 'Contatti' })
+  })
+  app.get('/analisi/:stock', (req, res) => {
+    res.render('analisi/analisi', { id: 'analisi', title: 'Analisi', isin: req.query.isin })
   })
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-import '../styles/style.css'
+// import '../styles/style.css'
 
 import mainMenu from '../components/shared/header/main-menu/main-menu.js'
 import cookieLayer from '../components/shared/cookie_layer/cookie_layer.js'
 import gotoTop from '../components/shared/goto_top/goto_top.js'
-import home from '../views/index.js'
+import index from '../views/index.js'
 
 mainMenu.toggleMobileMenu()
 cookieLayer.init()
 gotoTop.init()
-home.init()
+index.init()
