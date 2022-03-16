369b55350782e89f69198e7a3bb35ac19aa750d9
Marco Pavan <marco.pavan@vivabiocell.it>
2022-03-16 13:05:54 +0100

    Some minor changes

--- controllers/stocks.controller.js
+++ controllers/stocks.controller.js
@@ -2,7 +2,6 @@ const https = require('https')
 const options = {
   hostname: 'tradingradar.p.rapidapi.com',
   port: null,
-  path: '/api/stocks',
   headers: {
     'x-rapidapi-host': 'tradingradar.p.rapidapi.com',
     'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
@@ -13,26 +12,22 @@ const options = {
 // GET static stocks list
 exports.getStocksList = async (req, res) => {
   options.method = 'GET'
-
+  options.path = '/api' + req.url
   const request = https.request(options, function (response) {
     const chunks = []
-
     response.on('data', function (chunk) {
       chunks.push(chunk)
     })
-
     response.on('end', function () {
       const body = Buffer.concat(chunks)
       return res.status(response.statusCode).send(body.toString())
     })
   })
-
   request.on('error', error => {
     return res.status(500).send({
       code: 'serverError',
       message: error
     })
   })
-
   request.end()
 }
