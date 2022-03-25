d7581fbdde5f2eac7e39d0e3eea3529d225deb72
Marco Pavan <marco.pavan@vivabiocell.it>
2022-03-25 15:48:08 +0100

    Alerts section first work

--- views/analisi/analisi.js
+++ views/analisi/analisi.js
@@ -1,8 +1,10 @@
 import stockInfo from '../../components/analisi/stock_info/stock_info.js'
+import stockAlerts from '../../components/analisi/stock_alerts/stock_alerts.js'
 
 const analisi = {
   init: async () => {
-    await stockInfo.init()
+    const stockInfoData = await stockInfo.init()
+    await stockAlerts.init(stockInfoData)
   }
 }
 
