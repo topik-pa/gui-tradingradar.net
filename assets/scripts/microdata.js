dc93ddd388fdb99a592de33d36b99e055b31e4b4
Marco Pavan <marco.pavan@vivabiocell.it>
2022-09-02 10:53:14 +0200

    scripts in head revisited + first microdata

---
+++ assets/scripts/microdata.js
@@ -0,0 +1,16 @@
+{
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "url": "https://tradingradar.net",
+    "name": "tradingradar.net",
+    "description": "Analisi tecniche, Informazioni e Rassegna Stampa dei titoli del FTSE Mib",
+    "image": "https://www.tradingradar.net/assets/images/logo.png",
+    "author": {
+      "@type": "Person",
+      "name": "Marco Pavan"
+    },
+    "copyrightHolder": {
+      "@type": "Person",
+      "name": "Marco Pavan"
+    }
+}
