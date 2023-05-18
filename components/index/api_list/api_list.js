007ffaa90bd6250b19c96b544d23d3bb7432e061
Marco Pavan <marco.pavan@vivabiocell.it>
2023-05-18 15:46:35 +0200

    Parallel Api calls  + Some general UI

--- components/index/api_list/api_list.js
+++ components/index/api_list/api_list.js
@@ -2,6 +2,7 @@ import api from '../../../scripts/api.js'
 let $root, cls
 
 async function callTheApi (apis) {
+  const proms = []
   for (const apiCall of apis) {
     if (apiCall.category !== 'api') {
       continue
@@ -12,17 +13,27 @@ async function callTheApi (apis) {
     }
     apiCall.status = 'loading'
     updateUIStatus(apiCall)
-    try {
-      const request = await api.get(`api/stocks/${apiCall.name}/?${apiCall.qp}`)
-      apiCall.stocks = request.body
-      apiCall.status = 'success'
-    } catch (error) {
-      apiCall.status = 'error'
-      console.error(error)
-    }
-    updateUIStatus(apiCall)
-    printApiStocksInPage(apiCall)
+
+    proms.push(
+      new Promise((resolve, reject) => {
+        const res = api.get(`api/stocks/${apiCall.name}/?${apiCall.qp}`)
+        resolve(res)
+      })
+        .then((data) => {
+          apiCall.stocks = data.body
+          apiCall.status = 'success'
+          printApiStocksInPage(apiCall)
+        })
+        .catch((error) => {
+          apiCall.status = 'error'
+          console.error(error)
+        })
+        .finally(() => {
+          updateUIStatus(apiCall)
+        })
+    )
   }
+  await Promise.all(proms)
 }
 
 function updateUIStatus (apiCall) {
